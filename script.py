a24da89f3bb219c533a4151dbe52357297eea931
Stanislas Carlier <stan.carlier@orange.fr>
2022-04-04 15:30:08 +0200

    Recuperation titre page

--- script.py
+++ script.py
@@ -2,5 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'http://books.toscrape.com/catalogue/sharp-objects_997/index.html'
-reponse = requests.get(url)
-print(reponse)
+response = requests.get(url)
+page = response.content
+
+soup = BeautifulSoup(page, features='html.parser')
+
+title = soup.find('title')
+print(title.text)
