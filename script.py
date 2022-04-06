9efebd00f9cde1cc90db07dcba0846e62101f68c
Stanislas Carlier <stan.carlier@orange.fr>
2022-04-06 10:48:21 +0200

    Recuperation titre

--- script.py
+++ script.py
@@ -1,11 +1,42 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
+import csv
 
-url = 'http://books.toscrape.com/catalogue/sharp-objects_997/index.html'
+
+url = 'http://books.toscrape.com/catalogue/dark-notes_800/index.html'
 response = requests.get(url)
 page = response.content
 
-soup = BeautifulSoup(page, features='html.parser')
+soup = bs(page, features='html.parser')
 
-title = soup.find('title')
-print(title.text)
+articles = soup.find_all("article", class_="product_page")
+for article in articles:
+	title = article.h1.text
+	print(title)
+
+'''title = soup.findAll('h1', class_="col-sm-6 product_main")
+print(title)'''
+
+'''product_description = soup.findAll('article'="description")
+print(product_description)'''
+
+
+"""titles = soup.findAll('ol', class_="row")
+titles_text = []
+for title in titles:
+	titles_text.append(title.string)
+
+en_tete = ['titre']
+with open('data.csv', 'w') as fichier_csv:
+	writer = csv.writer(fichier_csv, delimiter=',')
+	writer.writerow(en_tete)
+	# zip permet d'itérer sur deux listes à la fois
+	for titles in zip(titles_text):
+		writer.writerow([titles])
+
+
+
+
+
+
+#descriptions = soup.find_all("p", class_="gem-c-document-list__item-description")"""
