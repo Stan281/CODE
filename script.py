29326cc813566a8c84458c766dc9e28e8ebffe0b
Stanislas Carlier <stan.carlier@orange.fr>
2022-04-25 11:28:38 +0200

    ajout url et img url

--- script.py
+++ script.py
@@ -8,6 +8,16 @@ response = requests.get(website_url)
 page = response.content
 soup = bs(page, features='html.parser')
 
+product_page_url = response.url
+print(product_page_url)
+
+universal_product_code = soup.find('table',class_='table table-striped')
+listetr = universal_product_code.find_all('tr')
+book_info={}
+for tr in listetr:
+	key = tr.th.text
+	value = tr.td.text
+	book_info[key]=value
 
 title = soup.find(class_='col-sm-6 product_main').h1
 print(title.text)
@@ -18,37 +28,20 @@ print(title.text)
 product_description = soup.find('head').find('meta', attrs={'name':'description'})
 print(product_description.attrs['content'].strip())
 
-universal_product_code = soup.find('table',class_='table table-striped')
-listetr = universal_product_code.find_all('tr')
-book_info={}
-for tr in listetr:
-	key = tr.th.text
-	value = tr.td.text
-	book_info[key]=value	
+img_url = soup.find("img")["src"]
+print(img_url)
 	
 #print(listetr)
 print(book_info)
 
-
-'''title  = soup.find_all('article', class_="product_page")
-for article in title :
-	title = article.h1.text
-	print(title)'''
-'''
-product_description = soup.find('h2', id='product_description')
-print(product_description)'''
-
-
-'''titles = soup.findAll('ol', class_="row")
-titles_text = []
-for title in titles:
-	titles_text.append(title.string)
-en_tete = ['titre']
 with open('data.csv', 'w') as fichier_csv:
 	writer = csv.writer(fichier_csv, delimiter=',')
 	writer.writerow(en_tete)
-	# zip permet d'itérer sur deux listes à la fois
-	for titles in zip(titles_text):
+
+
+
+# zip permet d'itérer sur deux listes à la fois
+'''for titles in zip(titles_text):
 		writer.writerow([titles])'''
 
 
