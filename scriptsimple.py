0e76f83409287f0f1f2185d022ee8f5bc0fc578f
Stanislas Carlier <stan.carlier@orange.fr>
2022-04-25 12:32:57 +0200

    test csv

--- scriptsimple.py
+++ scriptsimple.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-
 url = 'http://books.toscrape.com/catalogue/dark-notes_800/index.html'
 response = requests.get(url)
 
@@ -20,17 +19,15 @@ for article in book:
     price_excluding_tax = product_information_row[3].find("td").text
     number_available = product_information_row[5].find("td").text
     product_description = article.find("div", class_= "sub-header").findNext("p").text
+    #product_description = soup.select('article > p')[0].text
     review_rating = article.find("p", class_= re.compile(r'star-rating'))
     nb_rating = len(review_rating.find_all("i"))
-
-
 category = soup.find("ul", class_="breadcrumb")
 book_category = category.find_all("li")[-2].find("a").text
 
-
-with open('book.csv', 'w+') as outf:
-    outf.write('product_page_url, universal_ product_code (upc), title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url \n')
-    outf.write(url + ',' + upc_code + ',' + title + ',' + price_including_tax + ',' + price_excluding_tax + ',' + number_available + ',' + product_description + ',' + book_category + ',' +  str(nb_rating) + ',' + img_url + '\n')
+with open('book.csv', 'w+') as extract:
+    extract.write('product_page_url, universal_ product_code (upc), title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url \n')
+    extract.write(url + ',' + upc_code + ',' + title + ',' + price_including_tax + ',' + price_excluding_tax + ',' + number_available + ',' + product_description + ',' + book_category + ',' +  str(nb_rating) + ',' + img_url + '\n')
 
 
 
