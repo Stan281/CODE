56d1314b68a970ad3a17ec11c7595430ebc991e1
Stanislas Carlier <stan.carlier@orange.fr>
2022-04-25 11:43:32 +0200

    test csv

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import csv
-
+import pandas as pd
 
 website_url = 'http://books.toscrape.com/catalogue/dark-notes_800/index.html'
 response = requests.get(website_url)
@@ -19,27 +19,27 @@ for tr in listetr:
 	value = tr.td.text
 	book_info[key]=value
 
+print(book_info)
+
 title = soup.find(class_='col-sm-6 product_main').h1
 print(title.text)
-
-#product_description = soup.find('article', class_='product_page').find(id='product_description').next_sibling
-#print(product_description)
 
 product_description = soup.find('head').find('meta', attrs={'name':'description'})
 print(product_description.attrs['content'].strip())
 
 img_url = soup.find("img")["src"]
 print(img_url)
-	
-#print(listetr)
-print(book_info)
 
-with open('data.csv', 'w') as fichier_csv:
+'''df=pd.DataFrame(book_info,columns=['product_page_url','universal_product_code','product_type','price_excluding_tax','price_including_tax','tax','number_available','title','product_description','image_url'])
+df.to_csv('extractbook.csv')'''
+
+'''df=pd.DataFrame(book_info,columns=['product_page_url','universal_product_code','title','price_including_tax','price_excluding_tax','number_available','product_description','category','review_rating','image_url'])
+df.to_csv('extractbook.csv')'''
+
+
+'''with open('data.csv', 'w') as fichier_csv:
 	writer = csv.writer(fichier_csv, delimiter=',')
-	writer.writerow(en_tete)
-
-
-
+	writer.writerow(en_tete)'''
 # zip permet d'itérer sur deux listes à la fois
 '''for titles in zip(titles_text):
 		writer.writerow([titles])'''
